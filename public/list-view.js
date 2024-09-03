8dfc224d08634b9c0bb4854197c4a2e4abef4641
Owen Whitaker <owenwhitaker@gmail.com>
2024-09-03 17:54:35 -0500

    database is now connected to api, and front end can access data from db through api

--- public/list-view.js
+++ public/list-view.js
@@ -1,16 +1,13 @@
-//Here's the script to open a server with terminal: python3 -m http.server
-
-//list of churches to display
-//const churchList = ["Henderson Hills", "Life Church", "Wildwood", "Frontline", "First Baptist"]; //this list will be provided by the API
+//Access this page by serving index.js with node, then going to http://localhost:8000/list-view.html
 
 const listContainer = document.getElementById("listContainer");
 const testContainer = document.getElementById("testContainer"); // test container for testing get from api
 
 // Creates the visual list of churches on list-view.html, pulling list from api
-fetch('/api/get-church-list')
+/*fetch('/api/get-church-list')
     .then(response => response.json())
     .then(data => {
-        console.log(data.churches); // Console.log to see if I;m even getting data from the fetch
+        console.log(data.churches); // Console.log to see if I'm even getting data from the fetch
         data.churches.forEach(item => { 
             const newLink = document.createElement("a");
             newLink.textContent = item;
@@ -22,30 +19,21 @@ fetch('/api/get-church-list')
     })
     .catch(error => {
         console.error('Error fetching church list', error);
-    });
+    }); */
 
-// Creates the visual list of links on the listView HTML page
-/*churchList.forEach(item => { 
-    const newLink = document.createElement("a");
-    newLink.textContent = item;
-    newLink.href = ("detail-view.html?churchId=" + item); //change once have actual site
-    
-    listContainer.appendChild(newLink); //actually appends link to div
-
-}); */
-
-
-
-// Test Endpoint     ((TEST FETCH))
-fetch('/api/get-variable') 
+fetch('/api/get-churches')
     .then(response => response.json())
     .then(data => {
-        console.log(data.variableValue); //Access the variable's value
-        const fillFromAPI = document.createElement("p");
-        fillFromAPI.textContent = data.variableValue;
-        testContainer.appendChild(fillFromAPI);
+        console.log(data); // Console.log to see if I'm even getting data from the fetch
+        data.forEach(function(church) { 
+            const newLink = document.createElement("a");
+            newLink.textContent = church.name;
+            newLink.href = `detail-view.html?churchId=${church.id}`; //change once have actual site
+            
+            listContainer.appendChild(newLink); //actually appends link to div
+        
+        });
     })
     .catch(error => {
-        console.error('Error fetching variable', error);
+        console.error('Error fetching church data', error);
     });
-
